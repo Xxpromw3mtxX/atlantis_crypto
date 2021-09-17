a34edf44b0b89f45e62c9f8ebd697edfd58c4e38
Francesco Metta <fra.metta@hotmail.it>
2021-09-17 12:07:57 +0200

    it lang file

---
+++ atlantis_crypto/locales/it.lua
@@ -0,0 +1,9 @@
+Locales['it'] = {
+    ['farmprompt'] = 'Premi ~INPUT_CONTEXT~ per minare le monete',
+    ['farming'] = 'Minando...',
+    ['processprompt'] = 'Premi ~INPUT_CONTEXT~ per calcolare il tipo',
+    ['processing'] = 'Calcolando...',
+    ['crypto'] = 'monete crypto',
+    ['processingfull'] = 'Processo cancellato a causa di spazio insufficiente',
+    ['processingenough'] = 'Non hai abbastanza %s da calcolare! Devi averne almeno %s unità.',
+}
