fbc47b07cb1ea03efbffe9bb1d98d813ba02ca69
Francesco Metta <fra.metta@hotmail.it>
2021-09-17 12:11:51 +0200

    added english lang file

---
+++ atlantis_crypto/locales/en.lua
@@ -0,0 +1,9 @@
+Locales['it'] = {
+    ['farmprompt'] = 'Press ~INPUT_CONTEXT~ to mine crypto coins',
+    ['farming'] = 'Mining...',
+    ['processprompt'] = 'Press ~INPUT_CONTEXT~ to calculate the crypto type of coin',
+    ['processing'] = 'Calculating...',
+    ['crypto'] = 'crypto coin',
+    ['processingfull'] = 'Calculation canceled due to insufficient space',
+    ['processingenough'] = 'You don\'t have enough %s to calculate! You must have at least %s coins.',
+}
