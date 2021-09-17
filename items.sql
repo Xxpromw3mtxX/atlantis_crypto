0b72703183fabe04c4fa23b6386110590bd1da0c
Francesco Metta <fra.metta@hotmail.it>
2021-09-17 12:21:16 +0200

    Added sql items file

---
+++ items.sql
@@ -0,0 +1,5 @@
+INSERT INTO `items`(`name`, `label`, `weight`, `rare`, `can_remove`) VALUES 
+('crypto_coin','Crypto Coin','1','0','1'),
+('btc','Bitcoin','1','0','1'),
+('shiba','SHIBA INU','1','0','1')
+;
